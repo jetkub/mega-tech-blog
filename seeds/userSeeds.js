446e173720870a7008f2167dacdc179ba9a226f3
Jakub Cichon <jakub@uconn.edu>
2023-01-21 20:50:16 -0500

    remove full name of user in seed data

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -2,22 +2,18 @@ const User = require('../models/User');
 
 const userData = [
 	{
-		name: 'Jakub Cichon',
 		username: 'jakubpawel',
 		password: 'password1',
 	},
 	{
-		name: 'Ricky Aponte',
 		username: 'ricktastic12',
 		password: 'password2',
 	},
 	{
-		name: 'Ama Frema',
 		username: 'amaleaveitatthat',
 		password: 'password3',
 	},
 	{
-		name: 'Robert Obernier',
 		username: 'robbyrob',
 		password: 'password4',
 	},
