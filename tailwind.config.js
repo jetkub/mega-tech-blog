f1ce9b4175a9e9593cc5bbf947e29377f3447d11
Jakub Cichon <jakub@uconn.edu>
2023-01-22 01:58:57 -0500

    update tailwind stlye

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	content: ['./*.{html,js}'],
+	content: ['./views/**/*.{html,js,handlebars}'],
 	theme: {
 		extend: {
 			fontFamily: {
