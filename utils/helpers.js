1fef5658b44e89f55dc0bf36875d198707601fb2
Jakub Cichon <jakub@uconn.edu>
2023-01-23 02:18:47 -0500

    add timestamp formatter

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,8 @@ module.exports = {
 		// Format date as MM/DD/YYYY
 		return date.toLocaleDateString();
 	},
+	format_timestamp: (timestamp) => {
+		// format time as HH:MM:SS
+		return timestamp.toLocaleTimeString();
+	},
 };
