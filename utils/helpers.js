6b726a02390208d66090d142b3ca5a6dfe617bc0
Jakub Cichon <jakub@uconn.edu>
2023-01-29 21:22:15 -0500

    add truncation helper so that the full contents of a post aren't displayed on the homepage

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const Handlebars = require('handlebars');
+
 module.exports = {
 	format_date: (date) => {
 		// Format date as MM/DD/YYYY
@@ -6,5 +8,16 @@ module.exports = {
 	format_timestamp: (timestamp) => {
 		// format time as HH:MM:SS
 		return timestamp.toLocaleTimeString();
+	},
+	truncate: (str, len) => {
+		if (str.length > len && str.length > 0) {
+			var new_str = str + ' ';
+			new_str = str.substr(0, len);
+			new_str = str.substr(0, new_str.lastIndexOf(' '));
+			new_str = new_str.length > 0 ? new_str : str.substr(0, len);
+
+			return new Handlebars.SafeString(new_str + '...');
+		}
+		return str;
 	},
 };
