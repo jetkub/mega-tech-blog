1dd0bd16f4bbeee95adf66b6bbd9012d5f9c676f
Jakub Cichon <jakub@uconn.edu>
2023-01-29 21:45:05 -0500

    logout functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,11 @@
 const logout = async () => {
 	const response = await fetch('/api/users/logout', {
 		method: 'post',
+		body: JSON.stringify({}),
 		headers: { 'Content-Type': 'application/json' },
 	});
 	if (response.ok) {
-		document.location.replace('/');
+		response.redirect('/');
 		console.log(response);
 	} else {
 		alert(response.statusText);
