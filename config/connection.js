061d696a425902f52202389b2fe7006aba5750fd
Jakub Cichon <jakub@uconn.edu>
2023-01-29 21:35:07 -0500

    change logging/add comment

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,14 @@ if (process.env.JAWSDB_URL) {
 	sequelize = new Sequelize(
 		process.env.DB_NAME,
 		process.env.DB_USER,
-		process.env.DB_PASSWORD, {
+		process.env.DB_PASSWORD,
+		{
 			host: 'localhost',
 			dialect: 'mysql',
 			port: 3306,
-		});
+			logging: true, // set to false to avoid console.log spam
+		}
+	);
 }
 
 module.exports = sequelize;
