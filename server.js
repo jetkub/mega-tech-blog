5dfdb9ded0402d16dea9b1e7bac412f1da0b4156
Jakub Cichon <jakub@uconn.edu>
2023-01-30 00:02:47 -0500

    fix cookie security env variable

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-require('dotenv').config();
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -14,11 +14,13 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+const COOKIE_SECURE = process.env.COOKIE_SECURE === 'true' ? true : false;
+
 const sess = {
 	secret: 'sensitize-hurry7-pointing-moonshine',
 	cookie: {
 		maxAge: 5 * 60 * 1000, // 5 minutes -> 5 * 60 seconds * 1000 = 300000 milliseconds
-		secure: process.env.COOKIE_SECURE, // set to true if your using https/for production
+		secure: COOKIE_SECURE, // set to true if your using https/for production
 		httpOnly: true,
 		sameSite: 'strict',
 	},
