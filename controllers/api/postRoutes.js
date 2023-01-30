1cfb3c21a32e24ed1f24a0135bc774abc2b97ffa
Jakub Cichon <jakub@uconn.edu>
2023-01-29 19:59:54 -0500

    remove some commentend out lines, adjust some routes to use auth

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 			include: [
 				{
 					model: User,
-					attributes: ['username'],
+					attributes: ['username', 'id'],
 				},
 			],
 		});
@@ -43,7 +43,6 @@ router.get('/:id', async (req, res) => {
 				'title',
 				'created_at',
 				'updated_at',
-				// use raw MySQL aggregate function query to get a count of how many comments are on each post and return it under the name `comment_count`
 				[
 					sequelize.literal('(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)'),
 					'comment_count',
@@ -52,7 +51,7 @@ router.get('/:id', async (req, res) => {
 			include: [
 				{
 					model: User,
-					attributes: ['username'],
+					attributes: ['username', 'id'],
 				},
 				{
 					model: Comment,
@@ -73,7 +72,6 @@ router.get('/:id', async (req, res) => {
 });
 
 // POST /api/posts
-// router.post('/', async (req, res) => {
 router.post('/', withAuth, async (req, res) => {
 	// expects {title: 'title', post_content: 'big blob of text', user_id: 'number'}
 	try {
@@ -81,31 +79,31 @@ router.post('/', withAuth, async (req, res) => {
 			res.status(404).json({ message: 'No session found!' });
 			return;
 		}
+		console.log(req.body);
 		const postData = await Post.create({
-			title: req.body.title,
-			post_content: req.body.post_content,
-			user_id: req.session.user_id,
+			...req.body,
+			// add OR statement so that if the user is logged in, the user_id is the session user_id, otherwise it is the user_id from the request body
+			user_id: req.session.user_id || req.body.user_id,
 		});
-
+		console.log(postData);
 		res.status(200).json(postData);
 	} catch (err) {
+		console.log(err);
 		res.status(400).json(err);
 	}
 });
 
 // PUT /api/posts/:id
-// router.put('/:id', async (req, res) => {
 router.put('/:id', withAuth, async (req, res) => {
-	// expects {title: 'New Title', post_content: 'New Text' user_id: 1}
+	// expects {title: 'New Title', post_content: 'New Text'}
 	try {
 		if (!req.session) {
 			res.status(404).json({ message: 'No session found!' });
 			return;
 		}
-		postData = await Post.update(
+		const postData = await Post.update(
 			{
-				title: req.body.title,
-				post_content: req.body.post_content,
+				...req.body,
 			},
 			{
 				where: {
@@ -113,19 +111,19 @@ router.put('/:id', withAuth, async (req, res) => {
 				},
 			}
 		);
-
 		if (!postData) {
 			res.status(404).json({ message: 'No post found with this id' });
 			return;
 		}
+		console.log(postData);
 		res.status(200).json(postData);
 	} catch (err) {
+		console.log(err);
 		res.status(500).json(err);
 	}
 });
 
 // DELETE /api/posts/:id
-// router.delete('/:id', async (req, res) => {
 router.delete('/:id', withAuth, async (req, res) => {
 	try {
 		if (!req.session) {
@@ -137,6 +135,8 @@ router.delete('/:id', withAuth, async (req, res) => {
 				id: req.params.id,
 			},
 		});
+		console.log(req.body);
+		console.log(postData);
 
 		if (!postData) {
 			res.status(404).json({ message: 'No post found with this id!' });
